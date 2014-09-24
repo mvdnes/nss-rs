306e2902d3610d348fb5d2aed3a94b95c45d2421
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2014-09-24 14:15:34 +0200

    Refactored the example a bit

--- src/bin/nss2.rs
+++ src/bin/nss2.rs
@@ -2,20 +2,18 @@ extern crate nss;
 
 use nss::crypto::symm;
 
-fn main()
+fn doit() -> Result<(), String>
 {
-    nss::init().unwrap();
-
-    let mut crypt = symm::Crypter::new(symm::AES_128_CBC, true).unwrap();
+    let mut crypt = try!(symm::Crypter::new(symm::AES_128_CBC, true));
     let key : &[u8] = [0xe8, 0xa7, 0x7c, 0xe2, 0x05, 0x63, 0x6a, 0x31,0,0,0,0,0,0,0,0];
     let iv : &[u8] = [0xe4, 0xbb, 0x3b, 0xd3, 0xc3, 0x71, 0x2e, 0x58,0,0,0,0,0,0,0,0];
     let message = b"Encrypt me!\0";
 
     println!("input:     {}", String::from_utf8_lossy(message));
 
-    crypt.init(symm::Encrypt, key, iv).unwrap();
-    let mut out = crypt.update(message).unwrap();
-    out.extend(crypt.final().unwrap().into_iter());
+    try!(crypt.init(symm::Encrypt, key, iv));
+    let mut out = try!(crypt.update(message));
+    out.extend(try!(crypt.final()).into_iter());
 
     print!("encrypted:");
     for b in out.iter()
@@ -24,11 +22,20 @@ fn main()
     }
     println!("");
 
-    crypt.init(symm::Decrypt, key, iv).unwrap();
-    let mut dec = crypt.update(out.as_slice()).unwrap();
-    dec.extend(crypt.final().unwrap().into_iter());
+    try!(crypt.init(symm::Decrypt, key, iv));
+    let mut dec = try!(crypt.update(out.as_slice()));
+    dec.extend(try!(crypt.final()).into_iter());
 
     println!("decrypted: {}", String::from_utf8_lossy(dec.as_slice()));
 
+    Ok(())
+}
+
+fn main()
+{
+    nss::init().unwrap();
+    let result = doit();
+    let message = result.err().unwrap_or("Ok!".to_string());
+    println!("{}", message);
     let _ = nss::close();
 }
