1e76d5ab029fbacb91829ecf272dd0bb95334571
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2014-11-07 11:56:11 +0100

    New error showing using NSPR to string methods

--- src/ffi/nspr.rs
+++ src/ffi/nspr.rs
@@ -1,5 +1,6 @@
-use result::{NSSError, UNKNOWN};
+use result;
 use libc::c_uint;
+use std::c_str::CString;
 
 #[repr(C)]
 #[allow(dead_code)] // List all available options
@@ -34,10 +35,33 @@ pub enum PRBool
     PR_False = 0,
 }
 
-pub fn get_error_code() -> NSSError
+pub fn get_error_code() -> result::NSSError
 {
     let code = unsafe { PR_GetError() };
-    FromPrimitive::from_i32(code).unwrap_or(UNKNOWN)
+    result::NSS(code)
+}
+
+pub fn get_error_message(code: i32) -> Option<CString>
+{
+    if !error_code_exists(code) {
+        return None;
+    }
+
+    static LANG_EN : u32 = 1;
+    unsafe
+    {
+        let cmessage = PR_ErrorToString(code, LANG_EN);
+        Some(CString::new(cmessage, false))
+    }
+}
+
+fn error_code_exists(code: i32) -> bool
+{
+    if code >= 0 && code < 256 {
+        return true;
+    }
+
+    unsafe { !PR_ErrorToName(code).is_null() }
 }
 
 #[link(name="nspr4")]
@@ -47,4 +71,6 @@ extern "C"
                    maxPTDs: c_uint);
     pub fn PR_Cleanup() -> PRStatus;
     fn PR_GetError() -> i32;
+    fn PR_ErrorToString(code: i32, language: u32) -> *const ::libc::c_char;
+    fn PR_ErrorToName(code: i32) -> *const ::libc::c_char;
 }
